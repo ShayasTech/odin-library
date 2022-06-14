a915554353ad97b87113d7e939e46c4a36a16831
ShayasTech <shayas.tech@gmail.com>
2022-06-14 15:02:45 +0300

    Completed HTML and CSS, next Js

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,1 @@
 console.log("Digital Library");
-let addBtn = document.querySelector(".btn-add");
-let form = document.querySelector("form");
-
-addBtn.addEventListener("click", () => {
-    if (form.style.visibility == 'visible') {
-        addBtn.textContent = '➕ Add Books'
-        form.style.visibility = 'collapse';
-    }else{
-        addBtn.textContent = "➖ Add Books";
-        form.style.visibility = 'visible'
-    }
-});
-
